08f6385ad44dc3bbcfbd7208caf3527022578cde
Maarten de Vries <maarten@de-vri.es>
2021-11-06 14:03:41 +0100

    Do not write duplicate credentials, and add a trailing newline.

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,30 @@ function xdg_config_home() {
 	return `${os.homedir()}/.config`
 }
 
-async function run() {
-	const credentials = core.getInput('credentials', { required: true });
+function non_empty_trimmed_lines(input: string): string[] {
+	return input.split(/\r?\n/)
+		.map(line => line.trim())
+		.filter(line => line.length > 0);
+}
 
-	// Write credentials.
+async function run() {
+	const credentials = non_empty_trimmed_lines(core.getInput('credentials', { required: true }));
+
+	// Get the current credentials so we can avoid adding duplicates.
+	// On self-hosted runners, the credentials file could be retained between runs, so we don't want to add duplicates.
 	await fs.mkdir(`${xdg_config_home()}/git`, { recursive: true });
-	await fs.writeFile(`${xdg_config_home()}/git/credentials`, credentials, { flag: 'a', mode: 0o600 });
+	const file = await fs.open(`${xdg_config_home()}/git/credentials`, "a+", 0o600);
+	const old_credentials = non_empty_trimmed_lines((await file.readFile()).toString());
+	const new_credentials = credentials.filter(entry => !old_credentials.includes(entry));
+
+	// Replace the entire file, so it doesn't matter if it ended with a newline before.
+	file.truncate(0);
+	for (const credential of old_credentials) {
+		await file.write(credential + "\n");
+	}
+	for (const credential of new_credentials) {
+		await file.write(credential + "\n");
+	}
 
 	// Add git configuration.
 	await exec('git', ['config', '--global', 'credential.helper', 'store']);
